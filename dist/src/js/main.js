05e7375a16fd5ec05a004041e6f0bc7b35abddeb
Germán Pla <germanps@outlook.es>
2017-10-23 17:29:24 +0200

    Admin área fase1

--- dist/src/js/main.js
+++ dist/src/js/main.js
@@ -1,186 +1,27 @@
 
-var api = 'AIzaSyDLh29qSZwYCWUdqGtGIpHWD-5VMgyOIp0';
+$(document).ready(function() {
 
-function initMap() {
+	/*Area admin, carga contenido del menu lateral*/
+	var opt = $('.admin-options');
+	var content = $('#adminMaincontent div');
 
-	var latLng = {
-		lat: 41.3850639,
-		lng: 2.1734034999999494
-	}
-
-   var map = new google.maps.Map(document.getElementById('mapa'), {
-		'center': latLng,
-		'scrollwheel': false,
-		'zoom': 14,
-		'mapTypeId': google.maps.MapTypeId.ROADMAP
+	opt.click(function(e) {
+		var choosen = this.className.slice(14);	
+		content.each(function(index, el) {
+			if (el.className == choosen) {
+				$(this).show();
+			}else{
+				$(this).hide();
+			}
+		});
 	});
 
-    var content = '<h2> GDLWEBCAM </h2>' +
-    			  '<p>Del 10 al 12 de Diciembre</p>' + 
-    			  '<p>Visitanos!</p>';
-
-  	var marker = new google.maps.Marker({
-	   	position: latLng,
-	   	map: map,
-	   	title: 'GDLWEBCAM'
-    });
-
-    var info = new google.maps.InfoWindow({
-    	content: content
-    });
-
-    marker.addListener('click', function(){
-    	info.open(map, marker)
-    });
-}
-
-(function(){
-	"use strict";	
-
-	var gift = document.getElementById('gift');
-
-	document.addEventListener('DOMContentLoaded', function(){
-		//console.log('entra');
-		//Campos datos usuarios
-		var name = document.getElementById('name');
-		var surname = document.getElementById('surname');
-		var email = document.getElementById('email');
-
-		//Campos pases
-		var oneDayPass = document.getElementById('oneDayPass');
-		var twoDaysPass = document.getElementById('twoDaysPass');
-		var fullPass = document.getElementById('fullPass');
-
-		//Botones y divs
-		var calculate = document.getElementById('calculate');
-		var btnRegister = document.getElementById('btnRegister');
-		var error = document.getElementById('error');
-		var productsList = document.getElementById('productsList');
-		var totalAmount = document.getElementById('totalAmount');
-
-		//Extras
-		var sticker = document.getElementById('sticker');
-		var tshirt = document.getElementById('tshirt')
-
-		//Listeners
-		calculate.addEventListener('click', calculateAmount);
-		oneDayPass.addEventListener('blur', showDays); //evento quitar foco
-		twoDaysPass.addEventListener('blur', showDays); //evento quitar foco
-		fullPass.addEventListener('blur', showDays); //evento quitar foco
-
-		//Validations
-		name.addEventListener('blur', validateFields);
-		surname.addEventListener('blur', validateFields);
-		email.addEventListener('blur', validateFields);
-		email.addEventListener('blur', validateEmail);
-
-		//Functions
-		function validateEmail(e){
-			if (!this.value.indexOf("@")) {
-				error.style.display = 'none';
-				
-			}else{
-				error.style.color = 'red';
-				error.innerHTML = "Ingresa un email válido";
-				//this.focus();
-			}
-		}
-
-		function validateFields(){
-			if (this.value == '') {
-				error.style.display = 'block';
-				error.style.color = 'red';
-				error.innerHTML = "Este campo es obligatorio";
-				//this.focus();
-			}else{
-				error.style.display = 'none';
-			}
-		}
-
-		function calculateAmount(e){
-			e.preventDefault();
-
-			if (gift.value === '') {
-				alert('Debes elegir un regalo');
-				gift.focus();
-			}else{
-				var passDay = oneDayPass.value,
-					passTwoDays = twoDaysPass.value,
-					passfull = fullPass.value,
-					tshirtAmount = tshirt.value,
-					stickerAmount = sticker.value;
-
-
-				var totalPay = (passDay * 30) + 
-							   (passTwoDays * 45) + 
-							   (passfull * 50) +
-							   ((tshirtAmount * 10) * .93) + //descuento del 7%
-							   (stickerAmount * 2);
-
-				var list = [];
-				if (passDay >= 1) {
-					list.push(passDay + " Pases por día");
-				}
-				if (passTwoDays >= 1) {
-					list.push(passTwoDays + " Pases por 2 días");
-				}
-				if (passfull >= 1) {
-					list.push(passfull + " Pases por todos los días");
-				}
-				if (tshirtAmount >= 1) {
-					list.push(tshirtAmount + " Camisetas");
-				}
-				if (stickerAmount >= 1) {
-					list.push(stickerAmount + " Pegatinas");
-				}
-
-				productsList.style.display = 'block';
-				productsList.innerHTML = '';
-
-				for (var i = 0; i < list.length; i++) {
-					productsList.innerHTML += '<span>' + list[i] + '</span>';
-				}
-
-				totalAmount.innerHTML = "$ " + totalPay.toFixed(2);
-
-			}
-		}
-
-		function showDays(e){
-			var passDay = oneDayPass.value,
-				passTwoDays = twoDaysPass.value,
-				passfull = fullPass.value;
-			var chosenDays = [];
-			if(passDay > 0){
-				chosenDays.push('friday');
-				console.log(chosenDays);
-			}
-			if(passTwoDays > 0){
-				chosenDays.push('friday', 'saturday');
-				console.log(chosenDays);
-			}
-			if(passfull > 0){
-				chosenDays.push('friday', 'saturday', 'sunday');
-				console.log(chosenDays);
-			}
-			for (var i = 0; i < chosenDays.length; i++) {
-				document.getElementById(chosenDays[i]).style.display = 'block';
-			}
-		}
-
-	}); // DOM CONTENT LOADED
-})();
-
-
-jQuery(document).ready(function($) {
-	//Programa de Conferencias
-	$('.program-event .info-course:first').show();
-	$('.menu-program a').on('click', function(e) {
-		$('.menu-program a').removeClass('active');
-		$(this).addClass('active');
-		$('.hiden').hide();
-		var enlace = $(this).attr('href');
-		$(enlace).fadeIn(500);
-		return false;
+	/*Modals*/
+	$('.open-modal').click(function(e) {
+		if ($(this).attr('id') == "addUser") {
+			console.log($(this).attr('id'));
+			$('#userModal').show(100);
+		};
+		
 	});
 });
--- dist/view/admin.php
+++ dist/view/admin.php
@@ -6,3 +6,268 @@
         header('Location:../index.php');
     }
  ?>
+<!doctype html>
+<html class="no-js" lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>SqüalTshirts -- Shopping</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="../src/css/main.css">
+        <script src="../src/js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+    	<div class="admin-flex-container">
+			<aside class="admin-aside">
+				<ul class="admin-list">
+					<li class="admin-options dashboard">
+						<div class="admin-options-content">
+							<i class="fa fa-dashboard"></i>
+							<p>Dashboard</p>
+						</div>
+					</li>
+					<li class="admin-options users">
+						<div class="admin-options-content">
+							<i class="fa fa-users"></i>
+							<p>Usuarios</p>
+						</div>
+					</li>
+					<li class="admin-options products">
+						<div class="admin-options-content">
+							<i class="fa fa-product-hunt"></i>
+							<p>Productos</p>
+						</div>
+					</li>
+					<li class="admin-options categories">
+						<div class="admin-options-content">
+							<i class="fa fa-quora"></i>
+							<p>Categorias</p>
+						</div>
+					</li>
+					<li class="admin-options sales">
+						<div class="admin-options-content">
+							<i class="fa fa-eur"></i>
+							<p>Ventas</p>
+						</div>
+					</li>
+				</ul>
+			</aside>
+			<div class="admin-main">
+				<header class="admin-header">
+					<ul class="admin-header-list">
+						<li>
+							<h2 class="admin-title">SQUALTSHIRT</h2>
+						</li>
+						<li>
+							<div class="admin-info-header">
+								<i class="fa fa-user"></i>
+								<span class="admin-name"><?php echo $_SESSION['admin_user'] ?></span>
+								<span id="signOut" class="sign-out">Desconectar</span>
+							</div>
+						</li>
+					</ul>
+				</header>
+				<section id="adminMaincontent" class="admin-main-content">
+					<div class="dashboard">
+						dashboard
+					</div>
+					<div class="users">
+						<header class="users-header">
+							<h2>Usuarios</h2>
+							<a id="addUser" href='#' class='open-modal btn btn-danger btn-sm'>Agregar usuario</a>
+						</header>
+						<table class="results">
+							<thead>
+			            		<tr>
+			            			<th class='text-muted'>Item nº</th>
+			            			<th>ID usuario</th>
+			            			<th>Nombre</th>
+			            			<th>Apellido</th>
+			            			<th>Email</th>
+			            			<th>Password</th>
+			            			<th>Tipo usuario</th>
+			            			<th class="text-right">Acciones</th>
+			            		</tr>
+			            	</thead>
+			            	<tbody>
+			            		<?php 
+			            			$user_query = "select * from usuario order by id_usuario";
+			            			$usu_resul = $conexion->query($user_query);
+									$usu_rows = $usu_resul->num_rows;
+									$contador_usuarios = 1;
+									if ($usu_rows == 0) {
+										echo "No se encuentras usuarios en la base de datos";
+									}else{
+										while ($fila = $usu_resul->fetch_array()) {
+											extract($fila);
+											echo "<tr>
+													<td class='text-muted'>$contador_usuarios</td>
+													<td>$id_usuario</td>
+													<td>$nombre</td>
+													<td>$apellido</td>
+													<td>$email</td>
+													<td>$password</td>
+													<td>$tipo_usuario</td>
+													<td>
+														<a href='edit_usu.php?item=$id_usuario' class='btn btn-danger btn-sm'>Edit</a>
+														<a href='drop_usu.php?item=$id_usuario' class='btn btn-danger btn-sm'>Delete</a>
+	                                                </td>
+												 </tr>";
+												 $contador_usuarios++;
+										}
+									}
+			            		 ?>
+			            	</tbody>
+						</table>
+					</div>
+					<div class="products">
+						<header class="users-header">
+							<h2>Productos</h2>
+							<a id="addProduct" href='#' class='open-modal btn btn-danger btn-sm'>Agregar producto</a>
+						</header>
+						<table class="results">
+	                        <thead>
+	                            <tr>
+	                                <th class='text-muted'>Item nº</th>
+	                                <th>ID producto</th>
+	                                <th>Nombre</th>
+	                                <th>Descripción</th>
+	                                <th>Cantidad</th>
+	                                <th>Precio</th>
+	                                <th>Color</th>
+	                                <th>Talla</th>		                                
+	                                <th>Imagen</th>
+	                                <th>Categoria</th>
+	                                <th class="text-right">Acciones</th>
+	                            </tr>
+	                        </thead>
+	                        <tbody>
+                            <?php 
+                                //$product_query = "select * from productos";
+                                $product_query = "select p.id_camiseta, p.nombre_camiseta, p.descripcion, p.cantidad, p.imagen, p.precio, p.color, p.talla, c.nombre FROM camiseta p, categoria c where c.id_categoria = p.categoria_id_categoria order by id_camiseta;";
+                                $product_resul = $conexion->query($product_query);
+                                $product_rows = $product_resul->num_rows;
+                                $contador_camiseta = 1;
+                                if ($product_rows == 0) {
+                                    echo "No se encuentras productos en la base de datos";
+                                }else{
+                                    while ($fila_product = $product_resul->fetch_array()) {
+                                        extract($fila_product);
+
+                                        echo "<tr>
+                                                <td class='text-muted'>$contador_camiseta</td>
+                                                <td>$id_camiseta</td>
+                                                <td>$nombre_camiseta</td>
+                                                <td>$descripcion</td>
+                                                <td>$cantidad</td>
+                                                <td>$precio</td>
+                                                <td>$color</td>
+                                                <td>$talla</td>
+                                                <td>$imagen</td>
+                                                <td>$nombre</td>
+                                                <td> 
+                                                    <a href='edit_product.php?item=$id_camiseta' class='btn btn-danger btn-sm'>Edit</a>
+                                                    <a href='drop_product.php?item=$id_camiseta' class='btn btn-danger btn-sm'>Delete</a>
+                                                </td>
+                                             </tr>";
+                                             $contador_camiseta++;
+	                                    }
+	                                }
+	                             ?>
+	                        </tbody>
+						</table>
+					</div>
+					<div class="categories">
+						<header class="users-header">
+							<h2>Categorias</h2>
+							<a id="addCategory" href='#' class='open-modal btn btn-danger btn-sm'>Agregar categoria</a>
+						</header>
+						<table class="results">
+	                        <thead>
+	                            <tr>
+	                                <th class='text-muted'>Item nº</th>
+	                                <th>ID categoria</th>
+	                                <th>Nombre</th>
+	                                <th class="text-right">Acciones</th>
+	                            </tr>
+	                        </thead>
+	                        <tbody>
+	                            <?php 
+	                                $cat_query = "select * from categoria order by id_categoria";
+	                                $cat_resul = $conexion->query($cat_query);
+	                                $cat_rows = $cat_resul->num_rows;
+	                                $contador_cat = 1;
+	                                if ($cat_rows == 0) {
+	                                    echo "No se encuentras categorias en la base de datos";
+	                                }else{
+	                                    while ($fila_cat = $cat_resul->fetch_array()) {
+	                                        extract($fila_cat);
+	                                        echo "<tr>
+	                                                <td class='text-muted'>$contador_cat</td>
+	                                                <td>$id_categoria</td>
+	                                                <td>$nombre</td>
+	                                                <td>
+	                                                	<a href='edit_cat.php?item=$id_categoria' class='btn btn-danger btn-sm'>Edit</a>
+	                                                	<a href='drop_cat.php?item=$id_categoria' class='btn btn-danger btn-sm'>Delete</a>
+	                                                </td>
+	                                             </tr>";
+	                                             $contador_cat++;
+	                                    }
+	                                }
+	                             ?>
+	                        </tbody>
+	                        
+	                    </table>
+					</div>
+					<div class="sales">
+						<h2>Ventas</h2>
+						
+					</div>
+
+				</section>
+			</div>
+		</div>
+
+		<!--================================================ MODALS ================================================-->
+		<!--=========== Add User ===========-->
+		<div id="userModal" class="user-modal modal">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h2>Añadir usuario</h2>
+					<i class="fa fa-times" id="cerrar-modal"></i>
+				</div>
+				<form method='post' action='../controller/new_user.php' role="form" >
+	                <div class="modal-body">
+	                    <div class="form-group">
+	                        <label for="nameUserId">Nombre</label>
+	                        <input name="nameUser" type="text" id="nameUserId" placeholder="Nombre" class="form-control" required/>
+	                    </div>
+
+	                    <div class="form-group">
+	                        <label for="passwordUserId">Password</label>
+	                        <input name="passwordUser" type="text" id="passwordUserId" placeholder="Password" class="form-control" required/>
+	                    </div>
+
+	                    <div class="form-group">
+	                        <label for="tipoUserId">Tipo usuario => (0:admin # 1:normal)</label>
+	                        <input name="tipoUser" type="text" id="tipoUserId" placeholder="Tipo usuario" class="form-control" required/>
+	                    </div>
+
+	                </div>
+	                <div class="modal-footer">
+	                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+	                    <button type="submit" class="btn btn-primary">Añadir usuario</button>
+	                </div>
+	            </form>
+			</div>
+		</div>
+
+		<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../src/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="../src/js/plugins.js"></script>
+        <script src="../src/js/main.js"></script>
+	</body>
+</html>
