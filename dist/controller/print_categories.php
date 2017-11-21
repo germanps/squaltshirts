8a8b6f00deb32092178478c2ee856965363531ee
German <germanps@outlook.es>
2017-11-21 07:57:24 +0100

    Imprimir categorias aside pagina catalogo

---
+++ dist/controller/print_categories.php
@@ -0,0 +1,14 @@
+<?php 
+	$cat_query = "select * from categoria order by id_categoria";
+    $cat_resul = $conexion->query($cat_query);
+    $cat_rows = $cat_resul->num_rows;
+    if ($cat_rows == 0) {
+        echo "No se encuentras categorias en la base de datos";
+    }else{
+        while ($fila_cat = $cat_resul->fetch_array()) {
+            extract($fila_cat);
+            echo "<li>$nombre</li>";
+        }
+    }
+
+?>
--- dist/view/catalogo.php
+++ dist/view/catalogo.php
@@ -7,15 +7,23 @@
 
 <div id="mainGrid" class="main-grid container">
 	<aside id="leftContent" class="left-content">
-		aside
+		<div class="cat wrapper">
+			<div class="cat-container">
+				<h4>Filtrar</h4>
+				<div class="cat-categories">
+					<ul class="cat-list">
+					<?php 
+						include '../controller/print_categories.php';
+					?>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</aside>
 	<section id="mainContent" class="main-content">
 		contenido
 	</section>
 </div>
-
-
-
 
 <?php 
     include 'footer.php';
