5f1e83f4dba00c8500aac4052ca35ee7108241fa
germanps <germanps@outloo.es>
2017-12-22 12:01:57 +0100

    Control user email repetido

--- dist/controller/confirmar_pedido.php
+++ dist/controller/confirmar_pedido.php
@@ -28,7 +28,7 @@
 
 			//Hacemos el insert de la venta
 			$insert_venta = "insert into venta (id_venta, fecha, monto_final, descuento, usuario_id_usuario) values('null', CURRENT_TIMESTAMP, $total_compra, 0, $id_usuario);";
-			//$insert_venta_resul = $conexion->query($insert_venta);
+			$insert_venta_resul = $conexion->query($insert_venta);
 
 			//Cogemos el id del último insert de la tabla venta
 			$id_de_la_venta = mysqli_insert_id($conexion);
--- dist/view/new_account.php
+++ dist/view/new_account.php
@@ -6,8 +6,22 @@
 	$apellidos = $_POST['apellidos'];
 	$dni = $_POST['dni'];
 	$dir = $_POST['dir'];
-	$email = $_POST['email'];
+	$mail = $_POST['email'];
 	$pass = $_POST['password'];
+
+	//comporbar que el email no se repita en la base de datos
+	$email_user_query = "select email from usuario;";
+	$email_resul = $conexion->query($email_user_query);
+	$email_rows = $email_resul->num_rows;
+	while ($fila = $email_resul->fetch_array()) {
+		extract($fila);
+		if ($fila[0] == $mail) {
+			echo '<script type="text/javascript">
+					alert("El email ya está registrado, vuelve a probar")
+					window.location.assign("register.php");
+			     </script>';
+		}
+	}
 
 	//verificar formulario
 	$control = true;
@@ -20,19 +34,19 @@
   			$control = false;
   		}elseif ($dir == '') {
   			$control = false;
-  		}elseif ($email == '' or !preg_match("/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/",$_POST['email'])) {
+  		}elseif ($mail == '' or !preg_match("/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/",$_POST['email'])) {
   			$control = false;
   		}elseif ($pass == '') {
   			$control = false;
   		}
 
   		if ($control) {
-			$insert_user_query = "insert into usuario(id_usuario, nombre, apellido, dni, direccion, email, password, tipo_usuario) values('null','$nombre', '$apellidos', '$dni', '$dir', '$email', '$pass', 0)";
+			$insert_user_query = "insert into usuario(id_usuario, nombre, apellido, dni, direccion, email, password, tipo_usuario) values('null','$nombre', '$apellidos', '$dni', '$dir', '$mail', '$pass', 0)";
 			$insert_user_resul = $conexion->query($insert_user_query);
 			$conexion->close();	
 			//Iniciamos sesion
 			$_SESSION['usu_user'] = $nombre;
-			$_SESSION['usu_mail'] = $email;
+			$_SESSION['usu_mail'] = $mail;
 		}else{
 			echo '<script type="text/javascript">
 					window.location.assign("register_error.php");
@@ -44,7 +58,6 @@
 
 <div id="mainContent" class="home main-content register">
  	<div class="container">
- 		<h2>Formulario de registro</h2>
  		<div class="register-wrapper">
 			<div class="header-login-wrapper">
             	<h3>Usuario creado con éxito!</h3>
