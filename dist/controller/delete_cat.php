aeb5e06a8a84a8b420a97fab6487a724b145eb8c
German <germanps@outlook.es>
2018-01-24 14:57:37 +0100

    fix bug borrar cat

--- dist/controller/delete_cat.php
+++ dist/controller/delete_cat.php
@@ -4,13 +4,17 @@
 	if (isset($_SESSION['enter_ok'])) {	
 		$id = $_GET['item'];
 		$delete_query = "delete from categoria where id_categoria = $id";
-		$conexion->query($delete_query);
-		$conexion->close();
-		echo "Borrando categoria...";
-		header('Refresh: 3; url="../view/admin.php"');
+		if ($conexion->query($delete_query)) {
+			$conexion->close();
+			echo "Borrando categoria...";
+			header('Refresh: 3; url="../view/admin.php"');
+		}else{
+			$conexion->close();
+			echo "Esta categoria no se puede borrar porque tiene camisetas asociadas";
+			header('Refresh: 3; url="../view/admin.php"');
+		}
 	}else{
 		echo "Has llegado aqui de manera extraña...";
 		header('Refresh: 3; url="../index.php"');
 	}
-
  ?>
